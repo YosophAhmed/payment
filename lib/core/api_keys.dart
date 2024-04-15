c8dece7065d8dcb0b9ef9bdc0b3df8357987ba46
yosoph <devyosoph@gmail.com>
2024-04-15 09:45:56 +0200

    Latest Commit

--- lib/core/api_keys.dart
+++ lib/core/api_keys.dart
@@ -1,7 +1,3 @@
 class ApiKeys {
-  static String publishableKey =
-      'pk_test_51P2G2XP0ygrUHV9rDWLHcd7Z1js68vTb63BnRZJ9eRXGGdidXR1V1b55piwDhmIkRKsk9Qh1DyWligEDfhCidhXv00SJJsfwoL';
 
-  static String secretKey =
-      'sk_test_51P2G2XP0ygrUHV9rwVUISX6TIffrGs0aldc0P1zAaFsKnN1gW4TEccC8DOnC50pFB8pGZnhwkX0AebfnSBZGoOEW00p0xgnZb0';
 }
